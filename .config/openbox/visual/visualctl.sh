e45216c297aac7353af11a28ea2d8b2b1d4037ca
imsamuka <imsamuka@gmail.com>
2021-05-21 00:52:37 -0300

    updated visualctl.sh script and formatted

--- .config/openbox/visual/visualctl.sh
+++ .config/openbox/visual/visualctl.sh
@@ -1,47 +1,68 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 
-CUR_PATH=`dirname "$(readlink -f $0)"`
+
+
+# Initialize variables and functions
+CUR_PATH=`dirname "$(readlink -f "$0")"`
 CUR_FILE="$CUR_PATH/current"
-CUR_MODE="$(cat $CUR_FILE)"
+CUR_MODE=`cat $CUR_FILE || echo dark`
 
 
 set_visual() {
+
+
   # Make sure a visual mode name was given
   if [[ -z "$1" ]]; then
-    echo "Must provide visual mode name" 1>&2
+    echo "[visualctl] Must provide visual mode name" 1>&2
     exit 1
   fi
 
-  # Store the new visual value
-  echo "$1" > "$CUR_FILE"
 
-  # Set Wallpaper For this
+  # Logs
+  [[ "$CUR_MODE" == "$1" ]] \
+    && echo "[visualctl] Restarting visuals from '$CUR_MODE'" \
+    || echo "[visualctl] Changing visuals from '$CUR_MODE' to '$1'"
+
+
+  # Store the new visual value
+  echo "[visualctl] Saving the chosen visual..."
+  echo "$1" >| "$CUR_FILE"
+
+
+  # Set Wallpaper
+  echo "[visualctl] Setting Wallpaper..."
   "$CUR_PATH/set-wallpaper.sh" "$1"
 
+
   # Set Theme & UI
-  # ( "$VISMOD_DIR/mechanical/theme" && "$VISMOD_DIR/UI" ) &> /dev/null
+  echo "[visualctl] Setting GTK theme: TODO"
+  #( "$VISMOD_DIR/mechanical/theme" && "$VISMOD_DIR/UI" ) &> /dev/null
 
-  # Notify Success
-  #  notify-send -u low -i "$NOTIF_EYC_MIN_ICON" "Minimal Mode" "Eyecandy Theme enabled"
 
+  # Notify Results to User
+  echo "[visualctl] Notifying user: TODO"
+  #notify-send -u low -i "$NOTIF_EYC_MIN_ICON" "Minimal Mode" "Eyecandy Theme enabled"
+
+
+  # Exit
+  echo "[visualctl] Visuals set successfully."
   exit 0
 }
 
 
 
 case $1 in
-    start) # Initialize it
-      set_visual "$CUR_MODE"
+
+    # Toggle between configured options
+    toggle|"")
+      [[ $CUR_MODE != *"dark"* ]] && set_visual dark
+      [[ $CUR_MODE != *"light"* ]] && set_visual light
     ;;
-    toggle|"") # Toggle
-      if [[ $CUR_MODE != *"dark"* ]]; then
-          set_visual dark
-      elif [[ $CUR_MODE != *"light"* ]]; then
-          set_visual light # Change to light in the future
-      fi
-    ;;
-    *) # Choose any visual
-      set_visual "$1"
-    ;;
+
+    # Option to Initialize it
+    start|restart|refresh) set_visual "$CUR_MODE" ;;
+
+    # Choose any visual
+    *) set_visual "$1" ;;
 esac
