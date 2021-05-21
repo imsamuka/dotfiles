607dc143cfa26229ea3d6558c3b615210d907803
imsamuka <imsamuka@gmail.com>
2021-05-21 00:13:32 -0300

    heavy updates on set-wallpaper script

--- .config/openbox/visual/set-wallpaper.sh
+++ .config/openbox/visual/set-wallpaper.sh
@@ -1,5 +1,49 @@
-# Get the first argument given OR "dark" by default
-[[ -z "$1" ]] && FN="dark" || FN="$1"
+# Get the first argument given OR use "dark" by default
+[[ -z "$1" ]] && QUERY="dark" || QUERY="$1"
 
-# Find all wallpapers starting with that word, an put them in order as backgrounds
-feh --bg-fill --no-fehbg $(find -L ~/Images/Wallpapers -name "$FN*" -printf '%p ')
+
+
+# Find all wallpapers starting with that argument
+#   Supported Types: PNG JPEG GIF TIF (not by some limitation, add as please)
+#   Sorted order:    DEPTH -> ALPHABETICAL Dirs -> ALPHABETICAL Files
+#     /name1.jpg
+#     /name3.jpg
+#     /a/name5.jpg
+#     /c/name4.jpg
+#     /c/name6.jpg
+#     /a/b/name2.jpg
+#
+#   I think this regex is TOO much, but i couldn't find better a alternative
+echo "[set-wallpaper] Searching for '$QUERY'..."
+
+RES=`\
+find -L ~/Images/Wallpapers -type f -regextype posix-extended \
+-regex "[^ ]*/$QUERY[A-z0-9_-]*\.(png|jpe?g|gif|tiff?|PNG|JPE?G|GIF|TIFF?)$" \
+-printf '%d\0%h\0%p\n' | \
+sort -t '\0' -n | \
+awk -F '\0' '{print $3}'`
+
+
+
+# Exit if found nothing
+[[ -z "$RES" ]] \
+  && echo "No files found starting with '$QUERY' in ~/Images/Wallpapers/**" \
+  && exit 1
+
+
+
+# Logs
+echo "[set-wallpaper] Found these images:"
+
+[[ -x "$(command -v exa)" ]] \
+  && echo "$RES" | xargs exa -al --no-time --icons --color=always \
+  || echo "$RES" | xargs ls -Alf --color=always
+
+echo "[set-wallpaper] Setting Wallpapers..."
+
+
+
+# Put them in order as backgrounds - Each one will be in a monitor
+feh --bg-fill --no-fehbg $RES \
+  && echo "[set-wallpaper] Wallpapers set successfully." \
+  || echo "[set-wallpaper] Failed setting wallpapers." && exit 1
