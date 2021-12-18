2c4fb40f7e0aacc22660b297f171a006bd9f6943
imsamuka <imsamuka@gmail.com>
2021-12-17 21:51:13 -0300

    add more entries to settings menu

--- .scripts/particular/settings.sh
+++ .scripts/particular/settings.sh
@@ -7,7 +7,8 @@ echo '
 ^sep(  Settings)
 
 LxAppearance (GTK),lxappearance,gtk3-demo
-Qt5 Settings,qt5ct,QtIcon
+Kvantum (QT Theme),kvantummanager,kvantum
+Qt Settings,qt5ct,QtIcon
 Openbox Settings,obconf,obconf
 Tint2 Settings,tint2conf,tint2conf
 Display Settings,arandr,system-config-display
@@ -17,13 +18,16 @@ Firewall Settings,gufw,gufw
 ^sep(  Scripts)
 
 Refresh,refresh,gtk-refresh
+Toggle Compositor,toggle_compositor,compton
+Backup,st -t Backup -e tmux new-session -d "~/.scripts/particular/backup.sh" \; attach,grsync
 Display Hotplug,^term(hotplug_monitor),preferences-desktop-display-symbolic
 Wacom Hotplug,^term(hotplug_wacom),dcc_nav_wacom
 Icon Browser,icon_browser,gtk3-icon-browser
 
 ^sep(  System)
 
-Exit,^checkout(exit),system-devices-information
+Upgrade,st -t Upgrade -e tmux new-session -d "paru -Syu" \; attach,start-here-archlinux
+Exit,^checkout(exit),system-shutdown
 
 ^tag(exit)
 """^sep(   '$uptime')"""
