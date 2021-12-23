1e82d2cdb9267a76eea39cd036163e9202fd3234
imsamuka <imsamuka@gmail.com>
2021-12-23 11:24:01 -0300

    move Scripts field to submenu

--- .scripts/particular/settings.sh
+++ .scripts/particular/settings.sh
@@ -14,20 +14,20 @@ Tint2 Settings,tint2conf,tint2conf
 Display Settings,arandr,system-config-display
 Applications Settings,ezame,ezame
 Firewall Settings,gufw,gufw
+Scripts,^checkout(scripts),menu-editor
 
+^sep(  System)
+Backup,st -t Backup -e tmux new-session -d "~/.scripts/particular/backup.sh" \; attach,grsync
+Upgrade,st -t Upgrade -e tmux new-session -d "paru -Syyu" \; attach,start-here-archlinux
+Exit,^checkout(exit),system-shutdown
+
+^tag(scripts)
 ^sep(  Scripts)
-
 Refresh,refresh,gtk-refresh
 Toggle Compositor,toggle_compositor,compton
-Backup,st -t Backup -e tmux new-session -d "~/.scripts/particular/backup.sh" \; attach,grsync
 Display Hotplug,^term(hotplug_monitor),preferences-desktop-display-symbolic
 Wacom Hotplug,^term(hotplug_wacom),dcc_nav_wacom
 Icon Browser,icon_browser,gtk3-icon-browser
-
-^sep(  System)
-
-Upgrade,st -t Upgrade -e tmux new-session -d "paru -Syu" \; attach,start-here-archlinux
-Exit,^checkout(exit),system-shutdown
 
 ^tag(exit)
 """^sep(   '$uptime')"""
