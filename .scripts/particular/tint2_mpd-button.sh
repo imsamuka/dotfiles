f01d0b586d943332889fc765f084dbc739721e83
imsamuka <imsamuka@gmail.com>
2021-12-26 15:56:40 -0300

    add mpd button to tint2

---
+++ .scripts/particular/tint2_mpd-button.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+L_ICON="list-remove-all"
+P_ICON="player_play"
+S_ICON="player_pause"
+B_ICON="player_stop"
+X_ICON="action-unavailable"
+
+# current=$(basename "$(mpc current -f '[%title%]|[%file%]')")
+
+if [[ "$(mpc status)" == *"[playing]"* ]]; then
+    icon=$S_ICON
+    # prompt="Playing"
+elif [[ "$(mpc status)" == *"[paused]"* ]]; then
+    icon=$P_ICON
+    # prompt="Paused"
+elif  [[ "$(mpc status %length%)" == 0 ]]; then
+    icon=$L_ICON
+    # prompt="Empty"
+elif mpc &> /dev/null; then
+    icon=$B_ICON
+    # prompt="Stopped"
+else
+    icon=$X_ICON
+    # prompt="Unable"
+fi
+
+# echo $(icon_path --size 24 $icon)
+echo "/usr/share/icons/Papirus-Dark/24x24/actions/$icon.svg"
+# echo "$prompt"
+
---
+++ .scripts/particular/tint2_mpd-volume.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+mpc --wait volume $1 || exit 1
+
+volume=$(mpc volume | sed 's/volume: *//')
+
+dunstify \
+	-h int:value:${volume%'%'} \
+	-t 1000 -r 66 \
+	-i enjoy-music-player \
+	-a "Music Player Daemon" \
+	"MPD Volume"
+
